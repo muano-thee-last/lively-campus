79deb964af05f4d53b43da99eebf48596ca09c91
muano-thee-last <2573565@students.wits.ac.za>
2024-08-14 18:58:18 +0200

    Hello, World!

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders hello world", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloElement = screen.getByText(/Hello, World!/i);
+  expect(helloElement).toBeInTheDocument();
 });
