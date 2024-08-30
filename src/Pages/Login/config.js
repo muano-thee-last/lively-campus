01cd2ccf4168c57d8be3ea3c419cb4fcb591d4ed
= <=>
2024-08-31 00:07:34 +0200

    Fix API Issues

--- src/Pages/Login/config.js
+++ src/Pages/Login/config.js
@@ -4,28 +4,36 @@
 
   let app, auth, db;
 
-  async function initializeFirebase() {
-    try {
 
-      const firebaseConfig = {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY , 
-        authDomain: "witslivelycampus.firebaseapp.com",
-        databaseURL: "https://witslivelycampus-default-rtdb.firebaseio.com",
-        projectId: "witslivelycampus",
-        storageBucket: "witslivelycampus.appspot.com",
-        messagingSenderId: "61229245877",
-        appId: "1:61229245877:web:44c304d1f7eed94b9065fc"
-      };
 
-      // Initialize Firebase
-      app = initializeApp(firebaseConfig);
-      auth = getAuth(app);
-      db = getFirestore(app);
-      console.log("Firebase initialized successfully.");
-    } catch (error) {
-      console.error("Failed to initialize Firebase:", error);
+
+async function getData() {
+  const url = "https://us-central1-witslivelycampus.cloudfunctions.net/app/getEnvVar"; 
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
     }
+    const json = await response.json();
+    return json.value;
+  } catch (error) {
+    console.error(error.message);
   }
+}
+
+
+const key =  await getData();
+
+
+const firebaseConfig = {
+  apiKey: key,
+  authDomain: "witslivelycampus.firebaseapp.com",
+  databaseURL: "https://witslivelycampus-default-rtdb.firebaseio.com",
+  projectId: "witslivelycampus",
+  storageBucket: "witslivelycampus.appspot.com",
+  messagingSenderId: "61229245877",
+  appId: "1:61229245877:web:44c304d1f7eed94b9065fc"
+};
 
   initializeFirebase();
 
