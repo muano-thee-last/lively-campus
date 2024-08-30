1ec85d3b305812919a2e2f88339104fb036d378b
= <=>
2024-08-31 00:08:12 +0200

    fix API Issues

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log("Environment Variables:", process.env);
 
 root.render(
   <React.StrictMode>
