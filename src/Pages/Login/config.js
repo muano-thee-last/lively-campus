7f4f7174caeef56b3d3381e23e4ad5678d800916
= <=>
2024-08-30 20:21:33 +0200

    Fix API Issues

--- src/Pages/Login/config.js
+++ src/Pages/Login/config.js
@@ -4,19 +4,19 @@
 
   let app, auth, db;
 
-
   async function initializeFirebase() {
     try {
 
       const firebaseConfig = {
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.REACT_APP_FIREBASE_APP_ID
+        apiKey: process.env.REACT_APP_FIREBASE_API_KEY , 
+        authDomain: domain,
+        databaseURL: "https://witslivelycampus-default-rtdb.firebaseio.com",
+        projectId: "witslivelycampus",
+        storageBucket: "witslivelycampus.appspot.com",
+        messagingSenderId: "61229245877",
+        appId: "1:61229245877:web:44c304d1f7eed94b9065fc"
       };
+
       // Initialize Firebase
       app = initializeApp(firebaseConfig);
       auth = getAuth(app);
