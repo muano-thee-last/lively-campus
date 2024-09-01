7085e6070eee362986e8c11d58b2a63f3218c6a6
= <=>
2024-09-01 22:55:59 +0200

    Succes veryfyin

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import Home from "./Pages/Home/Home";
 import VerifyEmail from "./Pages/Login/verifyEmail";
-// Define the App component
 function App() {
   return (
-    // Top-level Router component
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
