bb7f9dc38200e89590026513e9c296c9997f29c0
Hulisani351 <trevorrambau10@gmail.com>
2024-09-02 13:44:25 +0200

    Add new features and fix bugs

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import EventsManagement from "./eventManagement/eventManagement"; // adjust the path if needed
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello, World!</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Hello, World!</h1>
+          <Link to="/events">
+            <button>Login</button>
+          </Link>
+        </header>
+        <Routes>
+          <Route path="/events" element={<EventsManagement />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
