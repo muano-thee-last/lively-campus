1e0034bc1304d7a7ced1083406776d9b244610e8
= <=>
2024-09-05 11:39:34 +0200

    add name in case it is not present

--- src/Pages/Login/createNewUser.js
+++ src/Pages/Login/createNewUser.js
@@ -1,32 +1,42 @@
 
 
-async function createNewUser(result){
+async function createNewUser(result) {
 
-    const userID = result.user.uid;
+  const userID = result.user.uid;
 
-    sessionStorage.setItem("uid", userID);
-    sessionStorage.setItem("user", JSON.stringify(result.user));
-    const user = {
-        UserID: userID,
-        FirstName: result.user.displayName,
-        LastName: result.user.displayName,
-        profile_picture: result.user.photoURL,
-        Email: result.user.email,
-      };
+  sessionStorage.setItem("uid", userID);
+  sessionStorage.setItem("user", JSON.stringify(result.user));
+  const user = {
+    UserID: userID,
+    FirstName: result.user.displayName,
+    LastName: result.user.displayName,
+    profile_picture: result.user.photoURL,
+    Email: result.user.email,
+  };
+  
+
+  if (!user.FirstName) {
+
+    const newFirstName = prompt("Please enter your name:");
+    if (newFirstName) {
+      user.FirstName = newFirstName;
+    } else {
+      alert("First name is required!");
+      return;
+    }
+  }
+
+  const createUserUrl = "https://us-central1-witslivelycampus.cloudfunctions.net/app/users";
+  await fetch(createUserUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  });
+  alert("Account successfully created");
 
 
-
-      const createUserUrl = "https://us-central1-witslivelycampus.cloudfunctions.net/app/users";
-      await fetch(createUserUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(user),
-      });
-      alert("Account successfully created");
-
-      
 
 
 }
