443b26330f1b0dddb11adf97d782d260ed530a87
Hulisani351 <127818291+Hulisani351@users.noreply.github.com>
2024-09-10 17:01:07 +0000

    Calendar (#21)
    * Calendar
    
    * yes
    
    * whatever
    
    ---------
    
    Co-authored-by: Hulisani351 <Hulisani351>
    Co-authored-by: Teboho02 <86244195+Teboho02@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import VerifyEmail from "./Pages/Login/verifyEmail";
 import Dashboard from "./Pages/dashboard/dashboard";
+
+import Calendar from "./Pages/Calendar/Calendar";
+
 import Notifications from './Pages/Notifications/NotificationsDashboard';
+
 import EventCreation from "./Pages/EventCreation/EventCreation";
 import Profile from "./Pages/Profile/Profile";
 import ViewMoreDetails from "./Pages/ViewMoreDetails/ViewMoreDetails";
@@ -17,6 +21,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
+        <Route path="/event-calendar" element={<Calendar />} />
         <Route path="/details/:id" element={<ViewMoreDetails />} /> {/* Add this line */}
         <Route path="/Notifications" element={<Notifications />} />
       </Routes>
