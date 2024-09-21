237a1b2776bb09720da0553efa0fe9aa134045b1
Amogelang Korase <amogelangkoras5@gmail.com>
2024-09-21 13:57:56 +0200

    when clicking manage events only the events that are yours appear. I used the organiser name since the organiser ID is null in the database

--- src/Pages/eventManagement/event-management-main-content.js
+++ src/Pages/eventManagement/event-management-main-content.js
@@ -1,22 +1,24 @@
 import React, { useRef, useEffect, useState } from 'react';
-import './event-management-main-content.css'
+import './event-management-main-content.css';
 import profile from './images-logos/profile-logo.jpg';
 import { FaSearch, FaEdit, FaTrash, FaCamera, FaUsers } from 'react-icons/fa';
 import useImageUpload from './useImageUpload';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 function EventManagementMainContent() {
   const [events, setEvents] = useState([]);
   const [editingEvent, setEditingEvent] = useState(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [uploadModalOpen, setUploadModalOpen] = useState(false); // Modal for image upload
-  const [selectedEventId, setSelectedEventId] = useState(null); // Store the eventId for image upload
+  const [currentUserName, setCurrentUserName] = useState('');
+  const [uploadModalOpen, setUploadModalOpen] = useState(false);
+  const [selectedEventId, setSelectedEventId] = useState(null); 
   const upcomingSlider = useRef(null);
+  const [error, setError] = useState(null);
 
   const {
     imagePreview,
     uploading,
     uploadImage,
-    error,
     fileInputRef,
     handleFileChange,
     handleDivClick,
@@ -24,21 +26,47 @@ function EventManagementMainContent() {
   } = useImageUpload();
 
   useEffect(() => {
-    fetch('https://us-central1-witslivelycampus.cloudfunctions.net/app/events')
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+    const fetchEvents = async () => {
+      try {
+        const auth = getAuth();
+
+        // Fetch current user's information from Firebase Auth
+        onAuthStateChanged(auth, (user) => {
+          if (user) {
+            setCurrentUserName(user.displayName);
+          } else {
+            setCurrentUserName('');
+          }
+        });
+
+        // Fetch all events from the API
+        try {
+          const response = await fetch('https://us-central1-witslivelycampus.cloudfunctions.net/app/events/');
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+
+          const data = await response.json();
+          setEvents(data);
+        } catch (fetchError) {
+          setError(fetchError.message);
         }
-        return response.json();
-      })
-      .then((data) => {
-        setEvents(data);
-        
-      })
-      .catch((error) => {
-        console.error('Error fetching events:', error);
-      });
+      } catch (error) {
+        setError(error.message);
+      }
+    };
+
+    fetchEvents();
   }, []);
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  // Filter events to include only those where organizerName matches the currentUserName
+  const validEvents = events.filter(
+    (event) => event.title !== 'Title not found' && event.organizerName === currentUserName
+  );
 
   const handleScroll = (slider, direction) => {
     if (slider.current) {
@@ -48,7 +76,6 @@ function EventManagementMainContent() {
     }
   };
 
- 
   const handleDelete = async (eventId) => {
     try {
       const response = await fetch(
@@ -78,8 +105,6 @@ function EventManagementMainContent() {
     setEditingEvent(null);
   };
 
-
-
   const handleSaveChanges = async () => {
     if (editingEvent) {
       try {
@@ -95,19 +120,18 @@ function EventManagementMainContent() {
               description: editingEvent.description,
               date: editingEvent.date,
               location: editingEvent.location,
-              imageUrl: editingEvent.imageUrl, // Include the image URL if it exists
+              imageUrl: editingEvent.imageUrl, 
             }),
           }
         );
 
         if (response.ok) {
-          // Update the events state with the updated event data
           setEvents(
             events.map((event) =>
               event.id === editingEvent.id ? { ...event, ...editingEvent } : event
             )
           );
-          setIsModalOpen(false); // Close the modal
+          setIsModalOpen(false); 
         } else {
           console.error('Failed to update event');
         }
@@ -119,46 +143,44 @@ function EventManagementMainContent() {
 
   const handleUploadImage = async () => {
     if (selectedEventId) {
-        const imageUrl = await uploadImage(); 
-        console.log(imageUrl); // This should return the URL of the uploaded image
-        if (imageUrl) {
-            try {
-                const response = await fetch(
-                    `https://us-central1-witslivelycampus.cloudfunctions.net/app/events/${selectedEventId}`,
-                    {
-                        method: 'PUT',
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                        body: JSON.stringify({ 
-                            imageUrl // Ensure this matches the server-side expected field
-                        }),
-                    }
-                );
-
-                if (response.ok) {
-                  setEvents(
-                    events.map((event) =>
-                        event.id === selectedEventId ? { ...event, imageUrl } : event
-                    )
-                );
-                    handleUploadModalClose(); // Close the modal on success
-                } else {
-                    console.error('Failed to update event image');
-                }
-            } catch (error) {
-                console.error('Error updating event image:', error);
+      const imageUrl = await uploadImage();
+      console.log(imageUrl); 
+      if (imageUrl) {
+        try {
+          const response = await fetch(
+            `https://us-central1-witslivelycampus.cloudfunctions.net/app/events/${selectedEventId}`,
+            {
+              method: 'PUT',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                imageUrl, 
+              }),
             }
-        }
-    }
-};
+          );
 
-  
+          if (response.ok) {
+            setEvents(
+              events.map((event) =>
+                event.id === selectedEventId ? { ...event, imageUrl } : event
+              )
+            );
+            handleUploadModalClose(); 
+          } else {
+            console.error('Failed to update event image');
+          }
+        } catch (error) {
+          console.error('Error updating event image:', error);
+        }
+      }
+    }
+  };
+
   const handleUploadModalClose = () => {
     setImagePreview(null);
     setUploadModalOpen(false);
     setSelectedEventId(null);
-    
   };
 
   return (
@@ -179,71 +201,73 @@ function EventManagementMainContent() {
           <button
             className="management-arrow-button left"
             onClick={() => handleScroll(upcomingSlider, 'left')}
-            
           >
             ‹
           </button>
           <div className="management-slider">
             <div className="management-card-container" ref={upcomingSlider}>
-              {events.map((event, index) => (
-                <div className="management-card" key={event.id}>
-                  <div className="management-card-first-row">
-                    <h4 className="management-event-title">{event.title}</h4>
-                  </div>
-                  <div className="management-card-second-row">
-                    <img
-                      src={profile}
-                      alt="Profile"
-                      className="management-profile-image"
-                    />
-                    <p className="management-event-organizer">{event.organizer}</p>
-                  </div>
-                  <div className="management-card-third-row">
-                    <img
-                      className="management-event-images"
-                      src={event.imageUrl}
-                      alt="Event"
-                    />
-                  </div>
-                  <div className="management-card-fourth-row">
-                    <div className="management-EventCapacity">
-                      <FaUsers
-                        className="management-attendance-icon"
-                        size={24}
-                        title="Attendance"
-                      />
-                      <p className="management-Capacity">{event.availableTickets}</p>
+              {validEvents.length > 0 ? (
+                validEvents.map((event) => (
+                  <div className="management-card" key={event.id}>
+                    <div className="management-card-first-row">
+                      <h4 className="management-event-title">{event.title}</h4>
                     </div>
+                    <div className="management-card-second-row">
+                      <img
+                        src={profile}
+                        alt="Profile"
+                        className="management-profile-image"
+                      />
+                      <p className="management-event-organizer">{event.organizerName}</p>
+                    </div>
+                    <div className="management-card-third-row">
+                      <img
+                        className="management-event-images"
+                        src={event.imageUrl}
+                        alt="Event"
+                      />
+                    </div>
+                    <div className="management-card-fourth-row">
+                      <div className="management-EventCapacity">
+                        <FaUsers
+                          className="management-attendance-icon"
+                          size={24}
+                          title="Attendance"
+                        />
+                        <p className="management-Capacity">{event.availableTickets}</p>
+                      </div>
 
-                    <div className="management-event-actions">
-                      <FaEdit
-                        className="management-event-edit"
-                        size={24}
-                        onClick={() => handleEdit(event)}
-                      />
-                      <FaCamera
-                        className="management-event-upload-image"
-                        size={24}
-                        onClick={() => {
-                          setSelectedEventId(event.id);
-                          setUploadModalOpen(true);
-                        }}
-                      />
-                      <FaTrash
-                        className="management-deleteEvent"
-                        size={24}
-                        onClick={() => handleDelete(event.id)}
-                      />
+                      <div className="management-event-actions">
+                        <FaEdit
+                          className="management-event-edit"
+                          size={24}
+                          onClick={() => handleEdit(event)}
+                        />
+                        <FaCamera
+                          className="management-event-upload-image"
+                          size={24}
+                          onClick={() => {
+                            setSelectedEventId(event.id);
+                            setUploadModalOpen(true);
+                          }}
+                        />
+                        <FaTrash
+                          className="management-deleteEvent"
+                          size={24}
+                          onClick={() => handleDelete(event.id)}
+                        />
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))
+              ) : (
+                <h1>No events found</h1>
+              )}
             </div>
           </div>
           <button
             className="management-arrow-button right"
             onClick={() => handleScroll(upcomingSlider, 'right')}
-            
           >
             ›
           </button>
@@ -257,7 +281,7 @@ function EventManagementMainContent() {
             <label>
               Title:
               <input
-                type="management-text"
+                type="text"
                 value={editingEvent.title || ''}
                 onChange={(e) =>
                   setEditingEvent({ ...editingEvent, title: e.target.value })
@@ -299,8 +323,6 @@ function EventManagementMainContent() {
               />
             </label>
 
-
-
             <div className="management-modal-buttons">
               <button onClick={handleModalClose}>Cancel</button>
               <button onClick={handleSaveChanges}>Save Changes</button>
@@ -309,36 +331,35 @@ function EventManagementMainContent() {
         </div>
       )}
 
-{uploadModalOpen && (
-       <div className="management-upload-modal-overlay">
-       <div className="management-upload-modal-content">
-         <h2>Upload Image</h2>
-         <input
-           type="file"
-           ref={fileInputRef}
-           onChange={handleFileChange}
-           style={{ display: 'none' }}
-           accept="image/*"
-         />
-         <div className="management-image-upload" onClick={handleDivClick}>
-           <span className="management-upload-placeholder">Select Image</span>
-         </div>
-         {imagePreview && (
-           <div className="management-image-previews">
-             <img src={imagePreview} alt="preview" className="management-image-preview" />
-           </div>
-         )}
-         {uploading && <p>Uploading...</p>}
-         {error && <p className="management-error">{error}</p>}
-         <div className="management-upload-modal-buttons">
-           <button onClick={handleUploadModalClose}>Cancel</button>
-           <button onClick={handleUploadImage} disabled={uploading}>
-             Upload
-           </button>
-         </div>
-       </div>
-     </div>
-     
+      {uploadModalOpen && (
+        <div className="management-upload-modal-overlay">
+          <div className="management-upload-modal-content">
+            <h2>Upload Image</h2>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+              accept="image/*"
+            />
+            <div className="management-image-upload" onClick={handleDivClick}>
+              <span className="management-upload-placeholder">Select Image</span>
+            </div>
+            {imagePreview && (
+              <div className="management-image-previews">
+                <img src={imagePreview} alt="preview" className="management-image-preview" />
+              </div>
+            )}
+            {uploading && <p>Uploading...</p>}
+            {error && <p className="management-error">{error}</p>}
+            <div className="management-upload-modal-buttons">
+              <button onClick={handleUploadModalClose}>Cancel</button>
+              <button onClick={handleUploadImage} disabled={uploading}>
+                Upload
+              </button>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
