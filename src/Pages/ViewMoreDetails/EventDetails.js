e1063c7e438feb74e5a9f967e0e908cc3ee30c38
Hulisani351 <127818291+Hulisani351@users.noreply.github.com>
2024-09-27 13:20:06 +0000

    Revert "Highlight own events and Details Fix"

--- src/Pages/ViewMoreDetails/EventDetails.js
+++ src/Pages/ViewMoreDetails/EventDetails.js
@@ -114,7 +114,7 @@ export default function EventDetails(){
         <div className="map-container">
           <iframe
             title={`Map showing location of ${event.venue}`}
-            src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsApiKey}&q=${encodeURIComponent(event.venue)}`}
+            src={`https://www.google.com/maps/embed/v1/place?key=${googleMapsApiKey}&q=${encodeURIComponent(event.location)}`}
             allowFullScreen
           ></iframe>
         </div>
